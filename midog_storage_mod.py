00c110444d3b04a2e16c635ba0b488c7eff78651
gwu-zymo <47827599+gwu-zymo@users.noreply.github.com>
2022-11-01 11:49:39 -0700

    Update midog_storage_mod.py

--- midog_storage_mod.py
+++ midog_storage_mod.py
@@ -26,7 +26,7 @@ oup = open('rawdata_midog.txt', 'w')
 for file in zipfile:
     fs = file.split('/')
     key = fs[1]
-    date = fs[-1].split('.')[2]
+    date = fs[-1].split('.')[-2]
     if key not in project:
         project[key] = [date]
     else:
@@ -36,10 +36,10 @@ for file in zipfile:
     os.system('unzip %s' % fs[-1])
     os.system('rm *.zip')
     f_path = '/'.join(fs[:-1])
-    s_path = '%s/miniseq.%s/' % (f_path, date)
+    s_path = 'tmp/%s/%s' % (f_path, date)
     for gz in os.listdir('./'):
         if gz.endswith('.gz'):
-            #s3.upload_file(gz, 'zymo-filesystem', '%s/%s' % (s_path, gz))
+            s3.upload_file(gz, 'zymo-filesystem', '%s/%s' % (s_path, gz))
             os.system('rm %s' % gz)
     oup.write('%s\t%s\n' % (key, s_path))
 oup.close()
