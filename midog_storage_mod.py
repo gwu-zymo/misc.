44ce46fc17bac1019ed4a0d8c09b8d3cd02d88a4
gwu-zymo <47827599+gwu-zymo@users.noreply.github.com>
2022-11-01 11:12:11 -0700

    Update midog_storage_mod.py

--- midog_storage_mod.py
+++ midog_storage_mod.py
@@ -36,11 +36,11 @@ for file in zipfile:
     os.system('unzip %s' % fs[-1])
     os.system('rm *.zip')
     f_path = '/'.join(fs[:-1])
-    s_path = '%s/miniseq.$s/%s' % (f_path, date, gz)
+    s_path = '%s/miniseq.$s/' % (f_path, date)
     for gz in os.listdir('./'):
-        s3.upload_file(gz, 'zymo-filesystem', s_path)
-        os.system('rm ')
-    oup.write('%s\t%s\n' % (key, ))
+        #s3.upload_file(gz, 'zymo-filesystem', '%s/%s' % (s_path, gz))
+        os.system('rm %s' % gz)
+    oup.write('%s\t%s\n' % (key, s_path))
 oup.close()
     
 s3.download_file('midog', 'Projects/md0392/rawdata/md0392.rawdata.220727.zip', 'md0392.rawdata.220727.zip')
