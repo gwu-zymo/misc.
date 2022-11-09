16d95a9a444bc0b20245d889af8ea3c23f767a35
gwu-zymo <47827599+gwu-zymo@users.noreply.github.com>
2022-11-08 16:50:53 -0800

    Update parse_interesting_sample.py

--- parse_interesting_sample.py
+++ parse_interesting_sample.py
@@ -1,1 +1,20 @@
+import os, sys
+
+key_words = ['oral', 'Oral', 'face', 'Face', 'mouth', 'Mouth', 'dental', 'Dental']
+
+spe_ct = {}
+
+inp = open('bunny_selected_samples.csv', 'r')
+line = inp.readline()
+header = line.strip().split(',')
+line = inp.readline()
+while line:
+  count = False
+  for key in key_words:
+    if key in line:
+      count = True
+      break
+  
+  if count == True:
+    ll = line.strip('\n').split(',')
 
