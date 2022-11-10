4ac4b37f8571c6a653ca4b236bbc3d962153c9e5
gwu-zymo <47827599+gwu-zymo@users.noreply.github.com>
2022-11-10 10:41:24 -0800

    Update transform_rgi.py

--- transform_rgi.py
+++ transform_rgi.py
@@ -1,3 +1,26 @@
 #transform rgi_amr_out.txt to a list of species with top AMR genes in each of them
 
+import operator
+
+#all[species] = {amr1: 0.9, amr2: 0.8}
+all = {}
+
 inp = open('rgi_amr_out.txt', 'r')
+line = inp.readline()
+while line:
+    ll = line.strip('\n').split('\t')
+    amr = ll[0]
+    for key in ll[1:]:
+        s_key = key.split('|')
+        spe = s_key[0]
+        pct = float(s_key[1])
+        ct = s_key[2]
+        spe_c = spe + '|' + ct
+        if not spe_c in all:
+            all[spe_c] = {}
+        all[spe_c][amr] = pct
+    line = inp.readline()
+inp.close()
+
+oup = open('spe_amr_list.txt', 'w')
+
