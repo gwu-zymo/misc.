23a78baec895f1a8bcd31f7dcbe9b530dcfd1d3f
gwu-zymo <47827599+gwu-zymo@users.noreply.github.com>
2022-11-10 10:57:35 -0800

    Update transform_rgi.py

--- transform_rgi.py
+++ transform_rgi.py
@@ -23,4 +23,10 @@ while line:
 inp.close()
 
 oup = open('spe_amr_list.txt', 'w')
-
+for spe in all:
+    oup.write('%s\t' % spe)
+    sorted_amr = sorted(all[spe].items(), key=operator.itemgetter(1))
+    for amr in sorted_amr:
+        oup.write('%s|%f\t' % (amr[0], amr[1]))
+    oup.write('\n')
+oup.close()
