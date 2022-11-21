382010d3e3ecf53e2e73c616fe2ffa1f4d897619
gwu-zymo <47827599+gwu-zymo@users.noreply.github.com>
2022-11-21 09:38:43 -0800

    Update check_missing_spe_db.py

--- check_missing_spe_db.py
+++ check_missing_spe_db.py
@@ -3,3 +3,31 @@ import os, sys
 kyle = {}
 inp = open('kyle.txt', 'r')
 while line:
+  kyle[line.strip('\n')] = ''
+  line = inp.readline()
+  line = inp.readline()
+inp.close()
+
+all = {}
+for file in os.listdir('./run/'):
+  key = file.split('.')[0]
+  all[key] = []
+  if key in kyle:
+    all[key].append('done')
+  else:
+    all[key].append('not')
+  all[key].append('1')
+
+
+for file in os.listdir('./run2/'):
+  key = file.split('.')[0]
+  if not key in all:
+    all[key] = []
+    if key in kyle:
+      all[key].append('done')
+    else:
+      all[key].append('not')
+  else:
+    all[key][-1] = all[key][-1] + '2'
+  
+oup = open('summary.txt', 'w')
