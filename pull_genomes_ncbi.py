3df787a737566f79758030018934e954e5ebeec0
gwu-zymo <47827599+gwu-zymo@users.noreply.github.com>
2022-11-30 09:44:20 -0800

    Create pull_genomes_ncbi.py

---
+++ pull_genomes_ncbi.py
@@ -0,0 +1,25 @@
+# download genomes
+
+import os, sys
+
+inp = open('core_set_562_modified.txt', 'r')
+all = {}
+line = inp.readline()
+while line:
+  key = line.strip('\n').replace('_genomic.fna', '')
+  all[key] = ''
+  line = inp.readline()
+inp.close()
+
+inp = open('assembly_summary_genbank.txt', 'r')
+line = inp.readline()
+while line:
+  for key in all:
+    if key in line:
+      path = line.split('\t')[19]
+      fpath = path + '_genomic.fna'
+      print(fpath)
+      os.system('wget %s' % fpath)
+      break
+  line = inp.readline()
+inp.close()
