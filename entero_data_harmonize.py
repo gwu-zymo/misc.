9b43b700a1c3bea4b5dba42717f524724549e990
gwu-zymo <47827599+gwu-zymo@users.noreply.github.com>
2023-06-07 10:48:50 -0700

    Update entero_data_harmonize.py

--- entero_data_harmonize.py
+++ entero_data_harmonize.py
@@ -9,7 +9,7 @@ inp = open('MetaHIT_SangerSamples.genus.txt', 'r')
 line = inp.readline()
 while line:
   genus = line.split('\t')[0]
-  genus_list[genus] = [line.strip('\n'), '']
+  genus_list[genus] = [line.strip('\n'), 0]
   line = inp.readline()
 inp.close()
 
@@ -27,9 +27,10 @@ line = inp.readline()
 while line:  
   ll = line.strip('\n').split('\t')
   genus = ll[0].split(';')[-1].lstrip('g__').split('-')
-  if (genus[0] in genus_list) or (genus[-1] in genus_list):
-    genus_list[1] = ll[sample_i]
-    total+=float(ll[sample_i])
+  for key in genus:
+    if key in genus_list: 
+      genus_list[key][1]+=float(ll[sample_i])/len(genus)
+      total+=float(ll[sample_i])/len(genus)
   line = inp.readline()
 inp.close()
 
@@ -37,7 +38,7 @@ genus_list['-1'][1] = str(1 - total)
 
 oup = open('sample_entero.data.txt', 'w')
 for genus in genus_list:
-  oup.write(genus_list[genus][0] + '\t' + genus_list[genus][1] + '\n')
+  oup.write(genus_list[genus][0] + '\t' + str(genus_list[genus][1]) + '\n')
 oup.close()
     
   
