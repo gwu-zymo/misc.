108681dcce06beb7289630b341682148846b0e2e
gwu-zymo <47827599+gwu-zymo@users.noreply.github.com>
2023-06-08 10:00:11 -0700

    Create batch_enterotype.py

---
+++ batch_enterotype.py
@@ -0,0 +1,26 @@
+#batch run enterotyping r script
+import os, sys, subprocess
+
+oup = open('entero.out', 'w')
+for zip in os.listdir('./'):
+  if file.endswith('.zip'):
+    subprocess.run(['unzip', zip])
+    folder = zip.rstrip('.zip')
+    subprocess.run(['cp', './%s/midog.a.Bac16Sv13/taxa_plots/sorted_otu_L6.txt' % folder, '.'])
+    inp = open('sorted_otu_L6.txt', 'r')
+    line = inp.readline()
+    line = inp.readline()
+    ll = line.strip('\n').split('\t')
+    for sample in ll[1:]:
+      if ('Pos' not in sample) and ('Neg' not in sample) and ('.Control' not in sample):
+        subprocess.run(['python3', 'entero_data_harmonize.py', sample])
+        inp_r = open('r_output.txt', 'r')
+        line = inp_r.readline()
+        while line:
+          oup.write('%s\t%s' % (sample, line))
+          line = inp_r.readline()
+        inp_r.close()
+    inp.close()
+
+
+
