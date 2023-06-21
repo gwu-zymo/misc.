33b08438780bc84804134651a4e5ea83dc7010d5
gwu-zymo <47827599+gwu-zymo@users.noreply.github.com>
2023-06-21 10:24:43 -0700

    Update parse_tax_abd_shotgun.py

--- parse_tax_abd_shotgun.py
+++ parse_tax_abd_shotgun.py
@@ -1,12 +1,12 @@
-#parse the sample species and genus abundance tables and prepare species list abundance tables
+#parse the sample species and genus abundance tables and prepare species list abundance tables for shotgun sourmash output
 #keystone list, pathogen list, pathway list, phylum ratios
-#use: python3 parse_tax_abd_lists.py folder sample_ID; for example: pbg0530.230511.zymo 1235364	
+#use: python3 parse_tax_abd_lists.py folder sample_ID; for example: in3773.230505 1BW	
 import os, sys
 
 folder = sys.argv[1]
 sample = sys.argv[2]
 species_abd = {}
-inp = open(f"./{folder}/midog.a.Bac16Sv13/taxa_plots/sorted_otu_L7.txt", 'r')
+inp = open(f"./{folder}/00...AllSamples.illumina.pe/Prokaryote/AbundanceTables/6.Species/species.txt", 'r')
 line = inp.readline()
 line = inp.readline()
 ll = line.strip('\n').split('\t')
