83a67f3d6e687d3e878b69885b90a422af4b01d3
gwu-zymo <47827599+gwu-zymo@users.noreply.github.com>
2023-06-23 13:12:07 -0700

    Update generate_healthy_pathway_abd.py

--- generate_healthy_pathway_abd.py
+++ generate_healthy_pathway_abd.py
@@ -2,6 +2,7 @@
 import os, sys
 
 pathway_all = {}
+pathway_abd = {}
 inp = open('pathway.txt', 'r')
 line = inp.readline()
 while line:
@@ -9,11 +10,35 @@ while line:
   taxa = ll[0]
   pathway = ll[1]
   if not pathway in pathway_all:
-    pathway_all[pathway] = []
+    pathway_all[pathway] = []  
   pathway_all[pathway].append(taxa)
   line = inp.readline()
 inp.close()
 
-
 inp = open('Healthy_Gut_Cohort_Species_Taxa_V1.csv', 'r')
+line = inp.readline()
+line = inp.readline()
+while line:
+  ll = line.strip('\n').split(',')
+  old_name = ll[0].split(';')[-1]
+  g = old_name.split(' ')[0].lstrip('s__').split('_')[0]
+  s = old_name.split(' ')[1].split('_')[0]
+  name = f"{g} {s}"
+  for pathway in pathway_all:
+    if name in pathway_all[pathway] or g in pathway_all[pathway]:
+      if pathway not in pathway_abd:
+        pathway_abd[pathway] = list(map(float, ll[1:]))
+      else:
+        pathway_abd[pathway] = [x + y for x, y in zip(pathway_abd[pathway], list(map(float, ll[1:])))]
+  line = inp.readline()
+inp.close()
+
+oup = open('Healthy_pathway.txt', 'w')
+for pathway in pathway_abd:
+  oup.write(f"{pathway}\t{'\t'.join(list(map(str, pathway_abd[pathway])))}\n")
+oup.close()
+
+
+
+
 
