47f9e13b38d9f8da4604b741726eda9431a8c4fb
gwu-zymo <47827599+gwu-zymo@users.noreply.github.com>
2023-06-23 12:50:04 -0700

    Update generate_healthy_pathway_abd.py

--- generate_healthy_pathway_abd.py
+++ generate_healthy_pathway_abd.py
@@ -1,2 +1,19 @@
 #take the healthy species abundance and generate healthy pathway ranges
+import os, sys
+
+pathway_all = {}
+inp = open('pathway.txt', 'r')
+line = inp.readline()
+while line:
+  ll = line.strip('\n').split('\t')
+  taxa = ll[0]
+  pathway = ll[1]
+  if not pathway in pathway_all:
+    pathway_all[pathway] = []
+  pathway_all[pathway].append(taxa)
+  line = inp.readline()
+inp.close()
+
+
+inp = open('Healthy_Gut_Cohort_Species_Taxa_V1.csv', 'r')
 
