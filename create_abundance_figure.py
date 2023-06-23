33eec59334f6b2726c97ff24701605df01885f2a
gwu-zymo <47827599+gwu-zymo@users.noreply.github.com>
2023-06-23 09:40:01 -0700

    Create create_abundance_figure.py

---
+++ create_abundance_figure.py
@@ -0,0 +1,41 @@
+#take in a sample abundance list table, such as keystone species, and generate the figures
+#need the healthy sample abundance table
+import matplotlib.pyplot as plt
+import numpy as np
+
+healthy_all = {}
+inp = open('Healthy_Gut_Cohort_Species_Taxa_V1.csv', 'r')
+line = inp.readline()
+line = inp.readline()
+while line:
+  ll = line.strip('\n').split('\t')
+  old_name = ll[0].split(';')[-1]
+  g = old_name.split(' ')[0].lstrip('s__').split('_')[0]
+  s = old_name.split(' ')[1].split('_')[0]
+  name = f"{g} {s}"
+  healthy_all[name] = map(float, ll[1:])
+  line = inp.readline()
+inp.close()
+
+inp = open(sys.argv[1], 'r')
+line = inp.readline()
+while line:
+  ll = line.strip('\n').split('\t')
+  spe = ll[0]
+  abd = float(ll[1])
+  if spe in healthy_all:
+    abd_list = healthy_all[spe].append(abd)
+    numbers = np.array(abd_list)
+    fig, ax = plt.subplots(figsize=(2, 12))
+    violin = ax.violinplot(numbers)
+    ax.plot(1, abd, 'ro')
+    plt.savefig(f"{species.replace(' ', '_')}_violin_plot.png")
+  line = inp.close()
+inp.close()
+
+
+
+
+
+
+  
