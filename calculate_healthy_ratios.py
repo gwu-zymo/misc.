e2d60fcd1713a05e3fddb53635b07353ed95d050
gwu-zymo <47827599+gwu-zymo@users.noreply.github.com>
2023-06-27 10:38:31 -0700

    Create calculate_healthy_ratios.py

---
+++ calculate_healthy_ratios.py
@@ -0,0 +1,31 @@
+#calculate the healthy range of phylum/genus ratios from healthy abundance table
+
+phylum_all = {}
+genus_all = {}
+
+inp = open('Healthy_Gut_Cohort_Species_Taxa_V1.csv', 'r')
+line = inp.readline()
+line = inp.readline()
+while line:
+  ll = line.strip('\n').split('\t')
+  phylum = ll[0].split(';')[1].lstrip('p__')
+  genus = ll[0].split(';')[-2].lstrip('g__')
+  if not phylum in phylum_all:
+    phylum_all[phylum] = list(map(float, ll[1:]))
+  else:
+    phylum_all[phylum] = [x + y for x, y in zip(phylum_all[phylum], list(map(float, ll[1:])))]
+  if not genus in genus_all:
+    genus_all[genus] = list(map(float, ll[1:]))
+  else:
+    genus_all[genus] = [x + y for x, y in zip(genus_all[genus], list(map(float, ll[1:])))]
+  line = inp.readline()
+inp.close()
+
+oup = open('healthy_ratios.txt', 'w')
+ratios = [x/y for x, y in zip(phylum_all['Firmicutes'], phylum_all['Bacteroidota'])]
+oup.write(f"Firmicutes|Bacteroidota\t{'\t'.join(list(map(str, ratios)))}\n")
+ratios = [x/y for x, y in zip(phylum_all['Proteobacteria'], phylum_all['Actinobacteriota'])]
+oup.write(f"Proteobacteria|Actinobacteriota\t{'\t'.join(list(map(str, ratios)))}\n")
+ratios = [x/y for x, y in zip(genus_all['Prevotella'], genus_all['Bacteroides'])]
+oup.write(f"Prevotella|Bacteroides\t{'\t'.join(list(map(str, ratios)))}\n")
+oup.close()
