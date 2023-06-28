567ba9137fbb2ba41b540ad079b0529242e6b220
gwu-zymo <47827599+gwu-zymo@users.noreply.github.com>
2023-06-28 14:26:07 -0700

    Update _run_gut_report_analysis.py

--- GMI_gut_report_amplicon/_run_gut_report_analysis.py
+++ GMI_gut_report_amplicon/_run_gut_report_analysis.py
@@ -2,7 +2,9 @@
 #need folder sample_ID; for example: pbg0530.230511.zymo 1235364
 import sys, subprocess
 
+subprocess.run(['sudo', 'apt', 'update', '-y'])
 
+subprocess.run(['unzip', ])
 
 result = subprocess.run(['command', 'arg1', 'arg2'])
 
