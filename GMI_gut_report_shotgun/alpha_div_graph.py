d8f93531816133d31287424f49c4b268608c1e83
gwu-zymo <47827599+gwu-zymo@users.noreply.github.com>
2023-07-19 12:02:23 -0700

    Create alpha_div_graph.py

---
+++ GMI_gut_report_shotgun/alpha_div_graph.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as mp
+import statistics
+
+#command: python3 {script} runID sampleID
+folder = sys.argv[1]
+SampleID = sys.argv[2]
+
+AlphaDivMedian = 229
+AlphaDivSample = 0
+
+sample_species_count = pd.read_csv('./%s/00...AllSamples.illumina.pe/Prokaryote/AlphaDiversity/6.Species/ObservedSp.csv')
+#print(sample_species_count)
+
+AlphaDivSample = sample_species_count.columns[1]
+print(AlphaDivSample)
+xax = ['Your Sample', 'Healthy Sample']
+yax = [int(AlphaDivSample), int(AlphaDivMedian)]
+
+mp.figure(figsize=(10,5))
+mp.bar(xax, yax, color = ['blue', 'orange'])
+mp.savefig('model_Alpha_Div.png', bbox_inches='tight', transparent=True)
+#mp.show
