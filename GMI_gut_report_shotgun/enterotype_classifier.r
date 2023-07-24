05809b32e0aef436b6dae77bce02656be4778f68
gwu-zymo <47827599+gwu-zymo@users.noreply.github.com>
2023-07-24 09:32:19 -0700

    Create enterotype_classifier.r

---
+++ GMI_gut_report_shotgun/enterotype_classifier.r
@@ -0,0 +1,44 @@
+###enterotype###
+#Uncomment next two lines if R packages are already installed
+install.packages("cluster", lib="./")
+library(cluster)
+
+#Download the example data and set the working directory
+#setwd('<path_to_working_directory>')
+data=read.table("sample_entero.data.txt", header=T, row.names=1, dec=".", sep="\t")
+data=data[-1,]
+
+dist.JSD <- function(inMatrix, pseudocount=0.000001, ...) {
+  KLD <- function(x,y) sum(x *log(x/y))
+  JSD<- function(x,y) sqrt(0.5 * KLD(x, (x+y)/2) + 0.5 * KLD(y, (x+y)/2))
+  matrixColSize <- length(colnames(inMatrix))
+  matrixRowSize <- length(rownames(inMatrix))
+  colnames <- colnames(inMatrix)
+  resultsMatrix <- matrix(0, matrixColSize, matrixColSize)
+  
+  inMatrix = apply(inMatrix,1:2,function(x) ifelse (x==0,pseudocount,x))
+  
+  for(i in 1:matrixColSize) {
+    for(j in 1:matrixColSize) { 
+      resultsMatrix[i,j]=JSD(as.vector(inMatrix[,i]),
+                             as.vector(inMatrix[,j]))
+    }
+  }
+  colnames -> colnames(resultsMatrix) -> rownames(resultsMatrix)
+  as.dist(resultsMatrix)->resultsMatrix
+  attr(resultsMatrix, "method") <- "dist"
+  return(resultsMatrix) 
+}
+
+data.dist=dist.JSD(data)
+
+pam.clustering=function(x,k) { # x is a distance matrix and k the number of clusters
+  require(cluster)
+  cluster = as.vector(pam(as.dist(x), k, diss=TRUE)$clustering)
+  return(cluster)
+}
+
+data.cluster=pam.clustering(data.dist, k=3)
+
+write.csv(data.cluster, 'sample_r_output.txt')
+                   
