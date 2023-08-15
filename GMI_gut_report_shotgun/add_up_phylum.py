ed71b365fda625b69cc3ab6fb1a3098d6521c73c
gwu-zymo <47827599+gwu-zymo@users.noreply.github.com>
2023-08-15 13:07:23 -0700

    Update add_up_phylum.py

--- GMI_gut_report_shotgun/add_up_phylum.py
+++ GMI_gut_report_shotgun/add_up_phylum.py
@@ -1,16 +1,24 @@
 #add up multiple lines of the same phylum
 
+sample = sys.argv[1]
+
 phylum_abd = {}
 inp = open('phylum.tsv', 'r')
 line = inp.readline()
 line = inp.readline()
+header = line.strip('\n').split('\t')[1:]
+index = header.index(sample)
 line = inp.readline()
 while line:
   ll = line.strip('\n').split('\t')
   phylum_name = ll[0].split(';')[-1].replace('p__', '').split('_')[0]
-  if not phylum_name in phylum_abd:
-    phylum_abd[phylum_name] = []
   values = [float(x) for x in ll[1:]]
-  phylum_abd[phylum_name] = [x + y for x, y in zip(phylum_abd[phylum_name], values)]
+  if not phylum_name in phylum_abd:
+    phylum_abd[phylum_name] = values
+  else:
+    phylum_abd[phylum_name] = [x + y for x, y in zip(phylum_abd[phylum_name], values)]
   line = inp.readline()
 inp.close()
+
+#to call the value of sample
+phylum_abd['Firmicutes'][index]
